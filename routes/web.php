a7eda40cf2fb9b75e04096af9be8aa4bb87598f1
okonueugene <97091138+okonueugene@users.noreply.github.com>
2022-10-20 16:20:07 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware'=> 'auth'], function () {
     Route::group(
