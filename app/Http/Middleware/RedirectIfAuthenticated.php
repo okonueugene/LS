3a8d732a71990bb4e7558450a09508d690ca2a4a
okonueugene <97091138+okonueugene@users.noreply.github.com>
2022-10-20 16:22:12 +0300

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return back();
+                return redirect('home');
             }
         }
 
