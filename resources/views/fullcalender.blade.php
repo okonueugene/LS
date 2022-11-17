9547fd0903c2d4bd6470cd51f8c2293e28d5d8dd
okonueugene <tylarrex67@gmail.com>
2022-11-17 10:08:05 +0300

    add calendar to site

--- resources/views/fullcalender.blade.php
+++ resources/views/fullcalender.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html>
 <head>
     <title>Scheduler</title>
@@ -9,7 +10,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
-    
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
 </head>
